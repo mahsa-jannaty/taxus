73468b2d06e2e8fbc4bea5c4babe7360d9987697
mahsa <mahsa.jannaty@gmail.com>
2018-08-28 17:46:12 +0430

    Project Scaffolding

---
+++ src/TaxusServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MahsaJannaty\Taxus;
+
+use Illuminate\Support\ServiceProvider;
+
+class TaxusServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        die('Hard!');
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
